4651c43659177ec1f2b7f1cc9adff926def0375d
Daniel Fontoura <dfontoura@outlook.com>
2022-03-02 19:24:53 -0300

    test: change cpf data

--- tests/cpf.test.ts
+++ tests/cpf.test.ts
@@ -1,13 +1,17 @@
-import Cpf from "../src/cpf";
+import Cpf from '../src/cpf';
+
+test('Should throw error if CPF is not informed', () => {
+    expect(() => new Cpf('')).toThrowError('CPF is required');
+})
 
 const invalidCpf = [
-    '',
     'not  number',
     '123.456.789-0',
-    '123.456.789-012',
+    '123.456.789-090',
     '111.111.111-11',
-    '05434886654',
-    '05434886670'
+    '123.456.789-00',
+    '123.456.789-90',
+    '12345678990'
 ];
 
 test.each(invalidCpf)('Invalid CPF: "%s". Should throw error.', (cpf) => {
@@ -15,13 +19,12 @@ test.each(invalidCpf)('Invalid CPF: "%s". Should throw error.', (cpf) => {
 });
 
 const validCpf = [
-    '05434886674',
-    '054.348.866-74',
-    '068.251.206-03',
+    '12345678909',
+    '123.456.789-09',
     '723.456.789-50'
 ];
 
 test.each(validCpf)('Valid CPF: "%s". Should return the CPF', (cpf) => {
     const newCpf = new Cpf(cpf);
-    expect(newCpf.getValue()).toBe(cpf);
+    expect(newCpf.getValue()).toBe(cpf.replace(/[^\d]+/g, ''));
 });
