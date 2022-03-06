406f0b806144e369980d306b990625e6d76dfd73
Daniel Fontoura <dfontoura@outlook.com>
2022-03-05 21:03:15 -0300

    feat: add coupon expiration date

--- src/coupon.ts
+++ src/coupon.ts
@@ -1,17 +1,26 @@
 export default class Coupon {
     private name: string;
     private percentage: number;
+    private expirationDate: Date;
 
-    constructor(name: string, percentage: number) {
-        if(!name || !percentage || percentage < 0 || percentage > 100) {
+    constructor(name: string, percentage: number, expirationDate: Date) {
+        if(!name || !percentage || percentage < 0 || percentage > 100 || !expirationDate) {
             throw new Error('Invalid parameters');
         }
 
         this.name = name;
         this.percentage = percentage;
+        this.expirationDate = expirationDate;
     }
 
     public getDiscount(total: number):  number  {
         return total * (this.percentage / 100);
+    }
+
+    public isValid(): boolean {
+        if (this.expirationDate < new Date()) {
+            return false;
+        }
+        return true;
     }
 }
