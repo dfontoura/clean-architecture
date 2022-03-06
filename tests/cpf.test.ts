f2e779b2e0666bc32896567c009a3877bf6ccaca
Daniel Fontoura <dfontoura@outlook.com>
2022-03-05 21:05:20 -0300

    test: improve readability

--- tests/cpf.test.ts
+++ tests/cpf.test.ts
@@ -1,30 +1,35 @@
 import Cpf from '../src/cpf';
 
-test('Should throw error if CPF is not informed', () => {
-    expect(() => new Cpf('')).toThrowError('CPF is required');
-})
+describe('Happy paths:', () => {
+    const validCpf = [
+        '12345678909',
+        '123.456.789-09',
+        '723.456.789-50'
+    ];
 
-const invalidCpf = [
-    'not  number',
-    '123.456.789-0',
-    '123.456.789-090',
-    '111.111.111-11',
-    '123.456.789-00',
-    '123.456.789-90',
-    '12345678990'
-];
-
-test.each(invalidCpf)('Invalid CPF: "%s". Should throw error.', (cpf) => {
-    expect(() => new Cpf(cpf)).toThrow(new Error("Invalid CPF"));
+    test.each(validCpf)('Valid CPF "%s": Should return the CPF', (cpf) => {
+        const newCpf = new Cpf(cpf);
+        expect(newCpf.getValue()).toBe(cpf.replace(/[^\d]+/g, ''));
+    });
 });
 
-const validCpf = [
-    '12345678909',
-    '123.456.789-09',
-    '723.456.789-50'
-];
+describe('Exception paths:', () => {
+    const invalidCpf = [
+        'not  number',
+        '123.456.789-0',
+        '123.456.789-090',
+        '111.111.111-11',
+        '123.456.789-00',
+        '123.456.789-90',
+        '12345678990'
+    ];
 
-test.each(validCpf)('Valid CPF: "%s". Should return the CPF', (cpf) => {
-    const newCpf = new Cpf(cpf);
-    expect(newCpf.getValue()).toBe(cpf.replace(/[^\d]+/g, ''));
+    test('CPF not informed: should throw error "CPF is required"', () => {
+        expect(() => new Cpf('')).toThrowError('CPF is required');
+    })
+
+
+    test.each(invalidCpf)('Invalid CPF "%s": should throw error "Invalid CPF".', (cpf) => {
+        expect(() => new Cpf(cpf)).toThrow(new Error('Invalid CPF'));
+    });
 });
