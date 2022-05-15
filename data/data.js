8f7958c89053ba09a55b2ff72da10f457b8847c5
Daniel Fontoura <dfontoura@outlook.com>
2022-05-15 13:10:43 -0300

    feat: create a data file

---
+++ data/data.js
@@ -0,0 +1,71 @@
+exports.products = [
+    {
+        id: 1,
+        name: 'Notebook',
+        price: 2000.00,
+        quantity: 2,
+        description: 'Notebook Dell Core i7 16GB 1TB'
+    },
+    {
+        id: 2,
+        name: 'Mouse',
+        price: 80.00,
+        quantity: 5,
+        description: 'Mouse Logitech'
+    },
+    {
+        id: 3,
+        name: 'Tablet',
+        price: 500.00,
+        quantity: 6,
+        description: 'Tablet Samsung 32GB'
+    },
+    {
+        id: 4,
+        name: 'Keyboard',
+        price: 80.00,
+        quantity: 4,
+        description: 'Keyboard Logitech'
+    },
+    {
+        id: 5,
+        name: 'Printer',
+        price: 500.00,
+        quantity: 3,
+        description: 'Printer HP'
+    },
+    {
+        id: 6,
+        name: 'Monitor',
+        price: 500.00,
+        quantity: 1,
+        description: 'Monitor Samsung'
+    }
+]
+
+exports.coupons = [
+    {
+        id: 1,
+        name: 'Desconto de 10%',
+        code: 'DESCONTO10',
+        value: 0.10,
+        min_value: 100.00,
+        expires_in: '2022-12-31'
+    },
+    {
+        id: 2,
+        name: 'Desconto de 20%',
+        code: 'DESCONTO20',
+        value: 0.20,
+        min_value: 200.00,
+        expires_in: '2022-12-31'
+    },
+    {
+        id: 3,
+        name: 'Desconto de 30%',
+        code: 'DESCONTO30',
+        value: 0.30,
+        min_value: 300.00,
+        expires_in: '2022-12-31'
+    }
+]
