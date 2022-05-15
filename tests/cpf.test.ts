ebe31fb2cba9a0be5119a81a53d53c174f675bde
Daniel Fontoura <dfontoura@outlook.com>
2022-05-15 13:08:32 -0300

    test: improve cpf test

--- tests/cpf.test.ts
+++ tests/cpf.test.ts
@@ -1,35 +1,23 @@
 import Cpf from '../src/cpf';
+import { CPF_NUMBERS } from './mock/cpf-numbers';
+
+const { validCpfNumbers, invalidCpfNumbers } = CPF_NUMBERS;
+
 
 describe('Happy paths:', () => {
-    const validCpf = [
-        '12345678909',
-        '123.456.789-09',
-        '723.456.789-50'
-    ];
-
-    test.each(validCpf)('Valid CPF "%s": Should return the CPF', (cpf) => {
+    test.each(validCpfNumbers)('Valid CPF "%s": Should return the CPF', (cpf) => {
         const newCpf = new Cpf(cpf);
         expect(newCpf.getValue()).toBe(cpf.replace(/[^\d]+/g, ''));
     });
 });
 
 describe('Exception paths:', () => {
-    const invalidCpf = [
-        'not  number',
-        '123.456.789-0',
-        '123.456.789-090',
-        '111.111.111-11',
-        '123.456.789-00',
-        '123.456.789-90',
-        '12345678990'
-    ];
-
     test('CPF not informed: should throw error "CPF is required"', () => {
         expect(() => new Cpf('')).toThrowError('CPF is required');
     })
 
 
-    test.each(invalidCpf)('Invalid CPF "%s": should throw error "Invalid CPF".', (cpf) => {
+    test.each(invalidCpfNumbers)('Invalid CPF "%s": should throw error "Invalid CPF".', (cpf) => {
         expect(() => new Cpf(cpf)).toThrow(new Error('Invalid CPF'));
     });
 });
